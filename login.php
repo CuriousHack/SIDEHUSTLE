fc4d153a10708757a9864ada5adbf57e0a984a7d
Akande Lateef <Akandelateef0@gmail.com>
2023-01-16 02:01:11 +0100

    Update login.php

--- login.php
+++ login.php
@@ -24,7 +24,7 @@
   		<button type="submit" class="btn" name="login_user">Login</button>
   	</div>
   	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
+  		Not a member? <a href="register.php">Sign up</a>
   	</p>
   </form>
 </body>
